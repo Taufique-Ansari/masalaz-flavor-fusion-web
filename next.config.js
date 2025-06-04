38e968b057c73a4cb144dd258f82c29b49a13ae0
Taufique-Ansari <taufiqueansari451@gmail.com>
2025-06-05 03:07:17 +0530

    chore: configure for GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/masalaz-flavor-fusion-web',
+  assetPrefix: '/masalaz-flavor-fusion-web/',
+}
+
+module.exports = nextConfig 
